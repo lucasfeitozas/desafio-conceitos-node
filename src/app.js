12642f3787db7d317feb9d5cdba46307869164ec
Lucas Feitozas <lucasfeitozas@gmail.com>
2020-04-07 21:13:55 -0300

    Resolucao do desafio de conceitos de node

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,97 @@ app.use(cors());
 
 const repositories = [];
 
+/**
+ * Middleware that validates if repository exists
+ */
+function checkIfRepositoryExists(req, res, next){
+  const { id } = req.params;
+
+  const index = repositories.findIndex(repository => repository.id === id);
+
+  if (index < 0) {
+    return res.status(400).send({"error": "Repository not exists"});
+  }
+
+  return next();
+}
+
+/**
+ * function to find repository
+ */
+function find(id){
+  return repositories.find(repository => repository.id === id);
+}
+/**
+ * Returns all repositories
+ */
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
+/**
+ * Request body: title, url, techs
+ * Create a new repository
+ */
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+
+  const repository = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.status(200).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+/**
+ * Request params: id;
+ * Request body: title, url, techs
+ * Update a repository
+ */
+app.put("/repositories/:id", checkIfRepositoryExists, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repository = find(id);
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.status(200).json(repository);
+
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+/**
+ * Request params: id;
+ * Delete a repository
+ */
+app.delete("/repositories/:id", checkIfRepositoryExists, (req, res) => {
+  const { id } = req.params;
+
+  const index = repositories.findIndex(repository => repository.id === id);
+  repositories.splice(index, 1);
+
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+/**
+ * Route params: id;
+ * Increase the count of likes
+ */
+app.post("/repositories/:id/like", checkIfRepositoryExists, (request, response) => {
+  const { id } = request.params;
+
+  const repository = find(id);
+  repository.likes +=1;
+  
+  return response.json(repository);
+
 });
 
 module.exports = app;
